396461507bb55f0a8c1b9d362ee5f4662dad5523
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-03-14 16:54:10 -0400

    added prompts and variables

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Adding variables for confirmations/prompts
+var confirmReady = confirm("Ready to select password criteria?");
+var promptLength = prompt("Enter desired password length (8-128 characters");
+
+// If the user is ready (confirmReady === true)
+if (confirmReady) {
+   alert(promptLength)
+}
+
+else {
+  alert("We'll see you next time!")
+}
+
+// Valuation of password length
+let length = document.getElementById("promptLength").value;
+
+// Creating character variables
+var lowercase = ("abcdefghijklmnopqrstuvwxyz")
+var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var numbers = (0123456789)
+var specialCharacters = ("#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
