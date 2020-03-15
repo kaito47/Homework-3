bdc62905e92eb9b959a7321f38ae94540f642e56
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-03-14 20:18:18 -0400

    initial input screen complete

--- script.js
+++ script.js
@@ -15,22 +15,42 @@ generateBtn.addEventListener("click", writePassword);
 
 // Adding variables for confirmations/prompts
 var confirmReady = confirm("Ready to select password criteria?");
-var promptLength = prompt("Enter desired password length (8-128 characters");
+var characterChoices = ["lowercase", "uppercase", "numbers", "specialCharacters"]
 
-// If the user is ready (confirmReady === true)
+
+// If the user is ready alert next steps
 if (confirmReady) {
-   alert(promptLength)
+   alert("You will be asked for password length and character type")
 }
 
+// If the new user is not ready goodbye 
 else {
   alert("We'll see you next time!")
+  e.preventDefault()
+  
+}
+// Prompt for user to enter password length
+var promptLength = prompt("Enter desired password length (8-128 characters");
+
+//  MAKE SURE INPUTS ARE VALIDATED
+if (promptLength) {
+   alert("Include" + "characterChoices")
+   for (i = 0; i < characterChoices.length; i++) {
+     console.log(characterChoices[i])
+   }
+   
 }
 
+
 // Valuation of password length
-let length = document.getElementById("promptLength").value;
+promptLength.value = passwordLength
 
 // Creating character variables
 var lowercase = ("abcdefghijklmnopqrstuvwxyz")
 var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-var numbers = (0123456789)
+var numbers = (0,1,2,3,4,5,6,7,8,9)
 var specialCharacters = ("#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+
+// Selecting which character variables to include
+
+// For loop for random character choice based on length 
